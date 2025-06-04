277bede239ed363ef004e3f015e92bb12f27bfd8
dangphuc282 <dangphuc28022002@gmail.com>
2025-06-04 10:06:28 +0900

    Add files via upload

---
+++ object1/main.js
@@ -0,0 +1,8 @@
+let person = {
+name: "john Doe",
+age: 25,
+job: "Developer"
+}
+console.log(`Ten toi la ${person.name}` );
+console.log(`Tuoi ${person.age}`);
+console.log(`Cong viec ${person.job}`);
